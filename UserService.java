307e7307e8b5a9e754069ae6b838ead78612ef35
boopoomofo <571041298@qq.com>
2024-11-21 11:15:55 +0800

    Fix: Replace NullPointerException with IllegalArgumentException (Fixes #1)

--- UserService.java
+++ UserService.java
@@ -1,6 +1,6 @@
 public class UserService {
     public void handleUser(Object user) {
-        if (user == null) throw new NullPointerException();
+        if (user == null) throw new IllegalArgumentException("User not found");
         System.out.println("Handling user: " + user);
     }
 }
